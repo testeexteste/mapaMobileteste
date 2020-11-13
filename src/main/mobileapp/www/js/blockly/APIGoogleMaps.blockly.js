417a774602bb98f2939855e9f4757ec07a8864cb
David Alves <david.alves@cronapp.io>
2020-11-13 15:18:27 +0000

    Atualização de versão da IDE

--- src/main/mobileapp/www/js/blockly/APIGoogleMaps.blockly.js
+++ src/main/mobileapp/www/js/blockly/APIGoogleMaps.blockly.js
@@ -32,6 +32,22 @@ window.blockly.js.blockly.APIGoogleMaps.Executar = function() {
 /**
  * Descreva esta função...
  */
+window.blockly.js.blockly.APIGoogleMaps.Adicionar_marcador = function() {
+ var item, destino, saida;
+  this.cronapi.maps.createMarker("map5590", 'cronappId', 'Sede CronApp', this.cronapi.maps.createLatLngPoint('-12.9992582', '-38.4690056'), '', '<h2 style=\"text-align: center;\">Sede do Cronapp em Salvador</h2>\n<h3 style=\"text-align: center;\"><a href=\"https://www.cronapp.io\" target=\"blank\"> https://www.cronapp.io </a></h3>', '');
+}
+
+/**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.APIGoogleMaps.Adiciona_circulo = function() {
+ var item, destino, saida;
+  this.cronapi.maps.drawCircle("map5590", 'IdCirculo', this.cronapi.maps.createLatLngPoint('-12.9992582', '-38.4690056'), '400', 'gray', 'black', '0.4', '');
+}
+
+/**
+ * Descreva esta função...
+ */
 window.blockly.js.blockly.APIGoogleMaps.GeraRota = function() {
  var item, destino, saida;
   destino = this.cronapi.screen.getScopeVariable('destino');
@@ -47,28 +63,12 @@ window.blockly.js.blockly.APIGoogleMaps.GeraRota = function() {
 /**
  * Descreva esta função...
  */
-window.blockly.js.blockly.APIGoogleMaps.Adicionar_marcador = function() {
- var item, destino, saida;
-  this.cronapi.maps.createMarker("map5590", 'cronappId', 'Sede CronApp', this.cronapi.maps.createLatLngPoint('-12.9992582', '-38.4690056'), 'cronapp-nuvem.png', '<h2 style=\"text-align: center;\">Sede do Cronapp em Salvador</h2>\n<h3 style=\"text-align: center;\"><a href=\"https://www.cronapp.io\" target=\"blank\"> https://www.cronapp.io </a></h3>', '');
-}
-
-/**
- * Descreva esta função...
- */
 window.blockly.js.blockly.APIGoogleMaps.AutoCompletarDestino = function() {
  var item, destino, saida;
   this.cronapi.maps.createAutoComplete("chegada1", 'address', this.cronapi.maps.createLatLngBounds(this.cronapi.maps.createLatLngPoint('-13.0183736\n', '-38.5480934'), this.cronapi.maps.createLatLngPoint('-12.7513579', '-38.1534927')), 'true', '', function(sender_item) {
       item = sender_item;
     this.cronapi.screen.createScopeVariable('destino', item);
   }.bind(this));
-}
-
-/**
- * Descreva esta função...
- */
-window.blockly.js.blockly.APIGoogleMaps.Adiciona_circulo = function() {
- var item, destino, saida;
-  this.cronapi.maps.drawCircle("map5590", 'IdCirculo', this.cronapi.maps.createLatLngPoint('-12.9992582', '-38.4690056'), '400', 'gray', 'black', '0.4', '');
 }
 
 /**
